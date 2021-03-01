366711eede48cc238b285646792d6972e4ef9f77
Marques <MBatoon1996@gmail.com>
2021-03-01 01:21:47 -1000

    add API call to request making a task active or complete

--- src/script.js
+++ src/script.js
@@ -40,6 +40,7 @@ class ToDoList extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.fetchTasks = this.fetchTasks.bind(this);
         this.deleteTask = this.deleteTask.bind(this);
+        this.toggleComplete = this.toggleComplete.bind(this);
     }
 
     componentDidMount() {
@@ -111,6 +112,26 @@ class ToDoList extends React.Component {
         })
     }
 
+    toggleComplete(id, completed) {
+        if(!id) {
+            return; // early return if no id
+        }
+        const newState = completed ? 'active' : 'complete';
+
+        fetch(`https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_${newState}?api_key=286`, {
+            method: "PUT",
+            mode: "cors",
+        }).then(checkStatus)
+        .then(json)
+        .then((data) => {
+            this.fetchTasks();
+        })
+        .catch((error) => {
+            this.setState({ error: error.message });
+            console.log(error);
+        })
+    }
+
     render() {
         const { new_task, tasks } = this.state;
 
@@ -124,6 +145,7 @@ class ToDoList extends React.Component {
                                 key={task.id} 
                                 task={task} 
                                 onDelete={this.deleteTask}
+                                onComplete={this.toggleComplete}
                                 />);
                         }) : <p>no tasks here</p>}
                         <form onSubmit={this.handleSubmit} className="form-inline my-4">
